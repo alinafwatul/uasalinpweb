c95221a3ae7a4e11abeed3a304982e8870dd0859
alinafwatul <alinafwatul@gmail.com>
2024-05-19 21:07:55 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALINNNN</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-[#141313]">
+    <nav class="flex mx-auto max-w-full py-5 justify-between fixed text-center items-center">
+        <div class="ml-28 space-x-5">
+            <a href="#home" class="text-[#577897] font-bold text-sm">HOME</a>
+            <a href="#about" class="text-white font-bold text-sm">ABOUT</a>
+            <a href="resume" class="text-white font-bold text-sm">RESUME</a>
+            <a href="" class="text-white font-bold text-sm">PORTFOLIO</a>
+        </div>
+    </nav>
+
+    <section name="home" id="home" class="max-w-full mx-auto flex py-48 px-0 justify-between">
+        <div class="ml-28">
+            <h1 class="text-6xl text-white">ALIN AFWATUL ULYA</h1>
+            <div class="bg-white text-white w-[600px] h-[10px] mt-5 rounded-sm text-center"></div>
+            <h2 class="text-4xl text-white text-center mt-2">Graphic Design</h2>
+            </div>
+    </section>
+
+    <section name="about" id="about" class="max-w-full mx-auto flex py-32 px-0 justify-between">
+        <div class="ml-28">
+            <img src="https://i.ibb.co/KGVByHz/IMG-7626-removebg-preview-1.png" alt="" class="w-[250px]">
+        </div>
+
+        <div class="text-left mx-auto">
+            <h1 class="text-white text-5xl font-bold underline">About</h1>
+            <div class="mt-10 text-white text-xl">
+            <p>Nama saya Alin Afwatul Ulya,</p>
+            <p>biasa dipanggil alin.</p>
+            <p>Saya seorang mahasiswa semester 4 di Politeknik Harapan Bersama Tegal</p>
+            <p>jurusan D3 Teknik Komputer.</p>
+            <p>Saya lahir di Tegal,pada 17 Oktober 2004.</p>         
+        </div>   
+        </div>
+    </section>
+
+    <section name="resume" id="resume" class="max-w-full mx-auto flex flex-wrap py-32 px-0 justify-between">
+        <div class="text-left mx-auto">
+            <h1 class="text-white text-2xl font-bold underline">SOFTWARE SKILLS</h1>
+            <div class="mt-3 text-white text-xl">
+                <h3>Adobe Photoshop</h3>
+                <h3>Corel Draw</h3>
+                <h3>Capcut</h3>
+                <h3>Canva</h3>
+            </div>
+        </div>
+
+        <div class="text-left mx-auto">
+            <h1 class="text-white text-2xl font-bold underline">LANGUANGE</h1>
+            <div class="mt-3 text-white text-xl">
+                <h3>Bahasa Indonesia</h3>
+                <h3>Bahasa  Jawa</h3>
+            </div>
+        </div>
+
+        <div class="text-left mx-auto">
+            <h1 class="text-white text-2xl font-bold underline">HOBBIEST</h1>
+            <div class="mt-3 text-white text-xl">
+                <h3>Membaca</h3>
+                <h3>Mendengarkan Musik</h3>
+                <h3>Menyanyi</h3>
+            </div>
+        </div>
+    </section>
+
+    <footer class="pb-10">
+        <div class="text-center text-xl text-white">
+            <h1 class="text-white">Contact me</h1>
+            <h3 class="">+62-8190-1125-492</h3>
+            <h3>alinafwatul@gmail.com</h3>
+        </div>
+
+    </footer>
+</body>
+</html>
